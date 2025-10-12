877f382d608614f65e3c04a3f5764548c4f5c529
nswbmw <4279697+nswbmw@users.noreply.github.com>
2025-10-12 14:50:37 +0800

    fix(types): use HoaMiddleware

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-import type { HoaContext } from 'hoa'
+import type { HoaContext, HoaMiddleware } from 'hoa'
 
 export type BodyparserOptions = {
   enableTypes?: Array<'json' | 'form' | 'text'>
@@ -15,8 +15,6 @@ export type BodyparserOptions = {
   useClone?: boolean
 }
 
-export type BodyparserMiddleware = (ctx: HoaContext, next: () => Promise<void>) => Promise<void>
-
-export function bodyParser(options?: BodyparserOptions): BodyparserMiddleware
+export function bodyParser(options?: BodyparserOptions): HoaMiddleware
 
 export default bodyParser
